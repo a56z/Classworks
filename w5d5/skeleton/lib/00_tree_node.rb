6482c2f654764c7220a8dd2aeb03568904d46ba3
Enrique Folte <hhfolte@gmail.com>
2022-02-09 00:54:36 -0500

    Add DeapthFirstSearch and BreadthFirstSearch methods in Searchable Module

--- w5d5/skeleton/lib/00_tree_node.rb
+++ w5d5/skeleton/lib/00_tree_node.rb
@@ -1,4 +1,36 @@
+module Searchable
+    def dfs(target_value = nil, &prc)
+        raise "Need a proc or target!" if [target_value, prc].none?
+        prc ||= Proc.new{  |node| node.value == target_value }
+
+        return self if prc.call(self)
+            
+            children.each do |child|
+                result = child.dfs(&prc)
+                return result unless result.nil?
+            end
+            nil
+    end
+
+    def bfs(target_value = nil, &prc)
+        raise "Need a proc or target!" if [target_value, prc].none?
+        prc ||= Proc.new{  |node| node.value == target_value }
+
+        nodes = [self]
+        until nodes.empty?
+            node = nodes.shift
+
+            return node if prc.call(node)
+            nodes.concat(node.children)
+        end
+        nil
+    end
+    
+end
+
+
 class PolyTreeNode
+    include Searchable
 
     attr_accessor :value
     attr_reader :parent
