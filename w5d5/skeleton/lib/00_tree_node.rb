0db27afca9ca52cad9ff4b77c386c61dde522445
Enrique Folte <hhfolte@gmail.com>
2022-02-08 23:43:26 -0500

    Implement a PolyTreeNode class

--- w5d5/skeleton/lib/00_tree_node.rb
+++ w5d5/skeleton/lib/00_tree_node.rb
@@ -2,18 +2,42 @@ class PolyTreeNode
 
     attr_accessor :value
     attr_reader :parent
-
     def initialize(value = nil)
         @value, @parent, @children = value, nil, []
     end
 
+    def children
+        @children.dup
+    end
+
     def parent=(parent)
-        @parent
         return if self.parent == parent
 
+        if self.parent
+            self.parent._children.delete(self)
+        end
+
         @parent = parent
-        self.parent._children << self << unless self.parent.nil?
+
+        self.parent._children << self unless self.parent.nil?
+
         self
     end
-end
+
+    def remove_child(child)
+        if child && !self.children.include?(child)
+            raise "You tried to remove a node that isn't a child!"
+        end
+        child.parent = nil
+    end
+
+    def add_child(child)
+        child.parent = self    
+    end
+
+    protected
+
+    def _children
+        @children
+    end
 end
