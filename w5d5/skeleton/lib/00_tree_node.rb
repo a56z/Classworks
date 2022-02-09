4fd4f2cf73adb03e93e80a9b76b7e3536ec48bd2
Enrique Folte <hhfolte@gmail.com>
2022-02-08 21:04:59 -0500

    Add initialize method

---
+++ w5d5/skeleton/lib/00_tree_node.rb
@@ -0,0 +1,15 @@
+class PolyTreeNode
+    def initialize
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent
+        @parent
+    end
+
+    def children
+        @children
+    end
+end
