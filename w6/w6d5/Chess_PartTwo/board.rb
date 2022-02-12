48ff57040e1dba1c7d34370165bafd751bf0a8a8
Enrique Folte <hhfolte@gmail.com>
2022-02-12 15:53:44 -0500

    Add grid logic for display.rb and method mov_cursor

--- w6/w6d5/Chess_PartTwo/board.rb
+++ w6/w6d5/Chess_PartTwo/board.rb
@@ -1,3 +1,5 @@
+require_relative 'cursor'
+
 class Board
     attr_reader :rows
 
--- w6/w6d5/Chess_PartTwo/display.rb
+++ w6/w6d5/Chess_PartTwo/display.rb
@@ -1,15 +1,13 @@
 require 'colorize'
-require_relative 'board'
 require_relative 'cursor'
 
 class Display
  def initialize(board)
     @board = board
+    @cursor = Cursor.new([0,0, board])
  end
 
- def render
-    @board = Board.new
- end
+
 
  def cursor
     @cursor = Cursor.new([0,0], board)
@@ -17,4 +15,32 @@ class Display
 #  def cursor_color
 #     @cursor_pos.colorize(:blue) 
 #  end
+
+    def move_cursor
+        render.each do |pos|
+            if pos.valid_pos?
+                render(pos)
+            end
+        end
+    end
+
+    def build_grid
+        @board.rows.each_with_index do |i, j|
+            build_row[i, j]
+        end
+    end
+
+    def build_row(row, 1)
+        row.map.each_with_index do |i, j|
+            color_options = colors_for(i, j)
+            piece.to_s.colorize(color, options)
+        end
+    end
+
+    def render
+        system("clear")
+        puts "Arrow keys, WASD, or vim to move, space or enter to confirm."
+        build_grid.each { |row| puts row.join }
+    end
+
 end
