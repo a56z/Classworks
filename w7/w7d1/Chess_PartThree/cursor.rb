ce1da48660b3371c78eabe0e5610cb6520f0c2c0
Enrique Folte <hhfolte@gmail.com>
2022-02-13 17:46:08 -0500

    Make final changes to chess game and start with TDD

--- w7/w7d1/Chess_PartThree/cursor.rb
+++ w7/w7d1/Chess_PartThree/cursor.rb
@@ -51,48 +51,35 @@ class Cursor
 
   private
 
-  def read_char
-    STDIN.echo = false # stops the console from printing return values
-
-    STDIN.raw! # in raw mode data is given as is to the program--the system
-                 # doesn't preprocess special characters such as control-c
-
-    input = STDIN.getc.chr # STDIN.getc reads a one-character string as a
-                             # numeric keycode. chr returns a string of the
-                             # character represented by the keycode.
-                             # (e.g. 65.chr => "A")
-
-    if input == "\e" then
-      input << STDIN.read_nonblock(3) rescue nil # read_nonblock(maxlen) reads
-                                                   # at most maxlen bytes from a
-                                                   # data stream; it's nonblocking,
-                                                   # meaning the method executes
-                                                   # asynchronously; it raises an
-                                                   # error if no data is available,
-                                                   # hence the need for rescue
-
-      input << STDIN.read_nonblock(2) rescue nil
-    end
-
-    STDIN.echo = true # the console prints return values again
-    STDIN.cooked! # the opposite of raw mode :)
-
-    return input
-  end
-
   def handle_key(key)
     case key
     when :ctrl_c
-        exit 0
-    when :space, :return
-        toggle_selected
-        cursor_pos
+      exit 0
+    when :return, :space
+      toggle_selected
+      cursor_pos
     when :left, :right, :up, :down
-        update_pos(MOVES[key])
-        nil
+      update_pos(MOVES[key])
+      nil
     else
-        puts key
+      puts key
     end
+  end
+
+  def read_char
+    STDIN.echo = false
+    STDIN.raw!
+
+    input = STDIN.getc.chr
+    if input == "\e"
+      input << STDIN.read_nonblock(3) rescue nil
+      input << STDIN.read_nonblock(2) rescue nil
+    end
+
+    STDIN.echo = true
+    STDIN.cooked!
+
+    input
   end
 
   def update_pos(diff)
