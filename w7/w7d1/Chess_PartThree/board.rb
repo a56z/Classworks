aa5ed4bf8c0df9077bd402e411fe110478a37939
Enrique Folte <hhfolte@gmail.com>
2022-02-12 21:11:57 -0500

    Add syntax corrections

--- w7/w7d1/Chess_PartThree/board.rb
+++ w7/w7d1/Chess_PartThree/board.rb
@@ -1,29 +1,25 @@
-require_relative 'cursor'
 require_relative 'pieces'
 
 class Board
     attr_reader :rows
 
     def initialize(fill_board = true)
-        # raise ArgumentError.new("Start position can't be empty!") if move_piece(start_pos.nil?)
-        # raise RuntimeError.new("Cannot move piece to desired position") if move_piece(end_pos.illegal_move?)
         @sentinel = NullPiece.instance
         make_starting_grid(fill_board)
-
     end
 
     def [](pos)
         raise 'invalid pos' unless valid_pos(pos)
        
         row, col = pos
-       @rows[row, col] 
+       @rows[row][col] 
     end
 
     def []=(pos, piece)
-        raise 'invalid pos' unless valid_pos(pos)
+        raise 'invalid pos' unless valid_pos?(pos)
 
         row, col = pos
-        @rows[row, col] = piece
+        @rows[row][col] = piece
     end
 
     def add_piece(piece, pos)
@@ -57,7 +53,7 @@ class Board
     #move without performing checks
     def move_piece!(start_pos, end_pos)
         piece = self[start_pos]
-        raise 'Piece cannot move like that' unless move_piece.moves.include?(end_pos)
+        raise 'Piece cannot move like that' unless piece.moves.include?(end_pos)
 
         self[end_pos] = piece
         self[start_pos] = sentinel
@@ -70,7 +66,7 @@ class Board
         @rows.flatten.reject(&:empty?)
     end
 
-    def valid_pos?
+    def valid_pos?(pos)
         pos.all? { |coord| coord.between(0, 7) }
     end
 
@@ -99,15 +95,6 @@ class Board
         king_pos || (raise 'King not found?')
     end
 
-    def make_starting_grid(fill_board)
-        @rows = Array.new(8) { Array.new(8, sentinel) }
-        return unless fill_board
-        %i(white black).each do |color|
-            fill_back_row(color)
-            fill_pawns_row(color)
-        end
-    end
-
     def in_check?(color)
         king_pos = find_king(color).pos
         pieces.any? do |p|
@@ -120,6 +107,15 @@ class Board
 
         pieces.select { |p| p.color == color }.all? do |piece|
             piece.valid_moves.empty?
+        end
+    end
+
+    def make_starting_grid(fill_board)
+        @rows = Array.new(8) { Array.new(8, sentinel) }
+        return unless fill_board
+        %i(white black).each do |color|
+            fill_back_row(color)
+            fill_pawns_row(color)
         end
     end
 
